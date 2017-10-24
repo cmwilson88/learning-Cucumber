6d70860efddd0b5ecf3cad1f7e4b9c55385740b2
Christopher Wilson <cmw.wilson88@gmail.com>
2017-10-23 20:27:24 -0400

    add second scenario for input field and capitalizing each letter

--- cucumber-react/src/test/features/example.feature
+++ cucumber-react/src/test/features/example.feature
@@ -3,5 +3,10 @@ Feature: Title check
 	and check its title
 
 Scenario: Get the title of webpage
-	Given I go to the website "/"
+	Given I go to the website /
 	Then I expect the title of the page Welcome to React
+
+Scenario: Fill input box
+	Given I go to the website /
+	When I fill out the input box with id of #maininput with the text Hello there
+	Then the subheader will read HELLO THERE
--- cucumber-react/src/test/features/step-definitions/example.js
+++ cucumber-react/src/test/features/step-definitions/example.js
@@ -6,8 +6,17 @@ defineSupportCode(function({Given, When, Then}) {
 		browser.url(url)
 	})
 
+	When('I fill out the input box with id of {text} with the text {text1}', function(text, text1) {
+		browser.addValue(text, text1)
+	})
+
 	Then('I expect the title of the page {text}', function(text) {
 		let pageHeading = browser.getText('h1')
 		assert.equal(text, pageHeading)
+	})
+
+	Then('the subheader will read {text}', function(text) {
+		let pageSubHeading = browser.getText('h3')
+		assert.equal(text, pageSubHeading)
 	})
 })
